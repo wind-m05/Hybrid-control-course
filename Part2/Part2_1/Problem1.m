66a55f73b0c69b44c41735d39c9d5d0904644862
Aerts <r.j.c.aerts@student.tue.nl>
2023-06-20 22:27:57 +0200

    c plot

--- Part2/Part2_1/Problem1.m
+++ Part2/Part2_1/Problem1.m
@@ -1,6 +1,7 @@
 clear
 close all
 clc
+set(0,'defaulttextInterpreter','latex')
 
 %% Dynamics
 A1 = [1.6 2; -4 -1.8];
@@ -24,14 +25,14 @@ ylim([-3,3])
 grid on
 
 %% c
-tau1 = 0:0.1:1.5;
-tau2 = tau1;
+tau1 = 0:0.01:1.5;
+tau2 = 0:0.01:1.5;
 
 stabAk = zeros(length(tau1),length(tau2),1);
 for p = 1:length(tau1)
     for q = 1:length(tau2)
-        Ak = exp(A2*tau2(q)) * exp(A1*tau1(p))
-        Ak = (eye(nx) + A2*tau2(q))*(eye(nx) + A1*tau1(p))
+        Ak = expm(A2*tau2(q)) * expm(A1*tau1(p));
+%         Ak = (eye(nx) + A2*tau2(q))*(eye(nx) + A1*tau1(p))
         eigAk = eig(Ak);
         if abs(eigAk(1)) < 1 && abs(eigAk(2)) < 1
             stabAk(p,q,:) = 1; % Schur stable
@@ -40,3 +41,17 @@ for p = 1:length(tau1)
         end
     end
 end
+
+%% Plot
+map = [0.9 0.1 0.1; 0.05 0.7 0.05];
+
+[Tau1, Tau2] = meshgrid(tau1, tau2);
+figure
+scatter(Tau1(:),Tau2(:),10,stabAk(:),'filled')
+colormap(map)
+colorbar('Ticks',[0 0.25 0.5 0.75 1],'TickLabels',{'','Unstable','','Stable'.''},'TickLabelInterpreter','latex')
+xlabel('Time in mode 1 $\tau_1$ [time units]')
+ylabel('Time in mode 2 $\tau_2$ [time units]')
+xlim([0 1.5])
+ylim([0 1.5])
+
