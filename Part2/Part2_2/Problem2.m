be53d8e2cd7887eb948d1c954843ad0f8a219bdb
Rody4534 <r.j.c.aerts@student.tue.nl>
2023-06-22 09:01:49 +0200

    ,

--- Part2/Part2_2/Problem2.m
+++ Part2/Part2_2/Problem2.m
@@ -15,14 +15,14 @@ nx = size(A1,1);
 %% a
 % LMI Variables
 Zvar = sdpvar(2,2);
-X12var = sdpvar(1,2);
-X21var = sdpvar(1,2);
+X1var = sdpvar(1,2);
+X2var = sdpvar(1,2);
 
 % Conditions
 Lp = Zvar >= 1e-9;
-Lf12 = [Zvar (Zvar*A1' + X12var'*B1'); (A1*Zvar + B1*X12var) Zvar] >= 0;
-Lf2 = [Zvar (Zvar*A2' + X21var'*B2'); (A2*Zvar + B2*X21var) Zvar] >= 0;
-L = [Lf12,Lf2,Lp]; 
+Lf1 = [Zvar (Zvar*A1' + X1var'*B1'); (A1*Zvar + B1*X1var) Zvar] >= 0;
+Lf2 = [Zvar (Zvar*A2' + X2var'*B2'); (A2*Zvar + B2*X2var) Zvar] >= 0;
+L = [Lf1,Lf2,Lp]; 
 
 % Solve
 opts = sdpsettings('solver','sdpt3');
@@ -45,12 +45,12 @@ Z = value(Zvar);
 P = Z\eye(nx); % faster than inv(Z)
 disp(eig(P))
 
-Y1 = value(X12var);
-Y2 = value(X21var);
-K1 = Y1*P;
-K2 = Y2*P;
-disp(eig(Z - (Z*A1' + Y1'*B1')*P*(A1*Z + B1*Y1)));
-disp(eig(Z - (Z*A2' + Y2'*B2')*P*(A2*Z + B2*Y2)));
+X1 = value(X1var);
+X2 = value(X2var);
+K1 = X1*P;
+K2 = X2*P;
+disp(eig(Z - (Z*A1' + X1'*B1')*P*(A1*Z + B1*X1)));
+disp(eig(Z - (Z*A2' + X2'*B2')*P*(A2*Z + B2*X2)));
 
 %% b Common QLF
 % observability
@@ -65,18 +65,18 @@ O2 = rref([C2; C2*A2]);
 % LMI Variables
 P1var = sdpvar(2,2);
 P2var = sdpvar(2,2);
-X11var = sdpvar(2,1);
-X12var = sdpvar(2,1);
-X21var = sdpvar(2,1);
-X22var = sdpvar(2,1);
+Y11var = sdpvar(2,1);
+Y12var = sdpvar(2,1);
+Y21var = sdpvar(2,1);
+Y22var = sdpvar(2,1);
 
 % Conditions
 Lp1 = P1var >= 1e-9;
 Lp2 = P2var >= 1e-9;
-Lf11 = [P1var (A1'*P1var - C1'*X11var'); (P1var*A1 - X11var*C1) P1var] >= 0;
-Lf12 = [P1var (A1'*P2var - C1'*X12var'); (P2var*A1 - X12var*C1) P2var] >= 0;
-Lf21 = [P2var (A2'*P1var - C2'*X21var'); (P1var*A2 - X21var*C2) P1var] >= 0;
-Lf22 = [P2var (A2'*P2var - C2'*X22var'); (P2var*A2 - X22var*C2) P2var] >= 0;
+Lf11 = [P1var (A1'*P1var - C1'*Y11var'); (P1var*A1 - Y11var*C1) P1var] >= 0;
+Lf12 = [P1var (A1'*P2var - C1'*Y12var'); (P2var*A1 - Y12var*C1) P2var] >= 0;
+Lf21 = [P2var (A2'*P1var - C2'*Y21var'); (P1var*A2 - Y21var*C2) P1var] >= 0;
+Lf22 = [P2var (A2'*P2var - C2'*Y22var'); (P2var*A2 - Y22var*C2) P2var] >= 0;
 L = [Lf11,Lf12,Lf21,Lf22,Lp1,Lp2]; 
 
 % Solve
@@ -101,12 +101,12 @@ disp(eig(P1))
 P2 = value(P2var);
 disp(eig(P2))
 
-X11 = value(X11var);
-X12 = value(X12var);
-X21 = value(X21var);
-X22 = value(X22var);
-L1 = inv(P2)*X12;
-L2 = inv(P1)*X21;
+Y11 = value(Y11var);
+Y12 = value(Y12var);
+Y21 = value(Y21var);
+Y22 = value(Y22var);
+L1 = inv(P2)*Y12;
+L2 = inv(P1)*Y21;
 disp(eig(P1 - (A1' - C1'*L1')*inv(P1)*(A1 - L1*C1)));
 disp(eig(P1 - (A1' - C1'*L1')*inv(P2)*(A1 - L1*C1)));
 disp(eig(P2 - (A2' - C2'*L2')*inv(P1)*(A2 - L2*C2)));
@@ -124,14 +124,14 @@ Pcls2var = sdpvar(4,4);
 % Conditions
 Lp1 = Pcls1var >= 1e-9;
 Lp2 = Pcls2var >= 1e-9;
-% Lf11 = Acls1'*Pcls1var*Acls1 - Pcls1var <= 0;
-% Lf12 = Acls1'*Pcls2var*Acls1 - Pcls1var <= 0;
-% Lf21 = Acls2'*Pcls1var*Acls2 - Pcls2var <= 0;
-% Lf22 = Acls2'*Pcls2var*Acls2 - Pcls2var <= 0;
-Lf11 = [Pcls1var Acls1'*Pcls1var; Pcls1var*Acls1 Pcls1var] >= 0;
-Lf12 = [Pcls1var Acls1'*Pcls2var; Pcls2var*Acls1 Pcls2var] >= 0;
-Lf21 = [Pcls2var Acls2'*Pcls1var; Pcls1var*Acls2 Pcls1var] >= 0;
-Lf22 = [Pcls2var Acls2'*Pcls2var; Pcls2var*Acls2 Pcls2var] >= 0;
+Lf11 = Acls1'*Pcls1var*Acls1 - Pcls1var <= 0;
+Lf12 = Acls1'*Pcls2var*Acls1 - Pcls1var <= 0;
+Lf21 = Acls2'*Pcls1var*Acls2 - Pcls2var <= 0;
+Lf22 = Acls2'*Pcls2var*Acls2 - Pcls2var <= 0;
+% Lf11 = [Pcls1var Acls1'*Pcls1var; Pcls1var*Acls1 Pcls1var] >= 0;
+% Lf12 = [Pcls1var Acls1'*Pcls2var; Pcls2var*Acls1 Pcls2var] >= 0;
+% Lf21 = [Pcls2var Acls2'*Pcls1var; Pcls1var*Acls2 Pcls1var] >= 0;
+% Lf22 = [Pcls2var Acls2'*Pcls2var; Pcls2var*Acls2 Pcls2var] >= 0;
 L = [Lf11,Lf12,Lf21,Lf22,Lp1,Lp2]; 
 
 % Solve
