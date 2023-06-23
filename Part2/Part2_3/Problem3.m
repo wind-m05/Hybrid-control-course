73cad0362d447fc6828813e149e59f856fa815d1
Aerts <r.j.c.aerts@student.tue.nl>
2023-06-23 15:02:05 +0200

    plot upgrade

--- Part2/Part2_3/Problem3.m
+++ Part2/Part2_3/Problem3.m
@@ -11,6 +11,7 @@ a = 0;
 
 tspan = [0 50];
 x0 = [0 0 0]';
+% x0 = [2 5 1]';
 
 %% b
 r = 1;
@@ -20,17 +21,45 @@ options = odeset('RelTol',1e-4,'AbsTol',1e-4);
 [t,x] = ode45(@(t,y) fun3(t,y,Ap,Bp,Cp,Dp,a,r,d), tspan, x0, options);
 [t_i,x_i] = ode45(@(t,y) int3(t,y,Ap,Bp,Cp,Dp,a,r,d), tspan, x0, options);
 
+e = zeros(length(x),1);
+e_i = zeros(length(x_i),1);
+for i = 1:length(x)
+    e(i) = -Cp*[x(i,1); x(i,2)] + Dp*r;
+end
+for i = 1:length(x_i)
+    e_i(i) = -Cp*[x_i(i,1); x_i(i,2)] + Dp*r;
+end
+
+%%
 figure
-subplot(3,1,1)
+subplot(4,1,1)
 plot(t,x(:,1)); grid on; hold on
 plot(t_i,x_i(:,1))
-ylabel('$x_{p,1}$')
-subplot(3,1,2)
+xline(1.6195,'--')
+xlim([0 25])
+ylim([-1.5 2.5])
+ylabel('$x_{p,1}(t)$')
+legend('SPAN','LTI integrator','interpreter','latex')
+
+subplot(4,1,2)
 plot(t,x(:,2)); grid on; hold on
 plot(t_i,x_i(:,2))
-ylabel('$x_{p,2}$')
-subplot(3,1,3)
+xline(1.6195,'--')
+xlim([0 25])
+ylabel('$x_{p,2}(t)$')
+
+subplot(4,1,3)
 plot(t,x(:,3)); grid on; hold on
 plot(t_i,x_i(:,3))
-ylabel('$x_{1}$')
+xline(1.6195,'--')
+xlim([0 25])
+ylim([-1.5 1.5])
+ylabel('$x_{1}(t)$')
+
+subplot(4,1,4)
+plot(t,e); grid on; hold on
+plot(t_i,e_i)
+xline(1.6195,'--')
+xlim([0 25])
+ylabel('$e(t)$')
 xlabel('Time $t$ [s]')
